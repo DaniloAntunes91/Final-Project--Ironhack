1285647e3ec5265b657fb587fe94d89f54c32e79
“Danilo <danilo.antunes91@gmail.com>
2023-07-25 14:50:09 +0200

    login&signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import IndexPage from './pages/indexpage';
 import LoginPage from './pages/loginPage';
-// import Layout from './Layout';
+import Layout from './Layout';
+import RegisterPage from './pages/Registerpage';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:4000'
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Layout />}> */}
+      <Route path="/" element={<Layout />} >
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
-     
+        <Route path="/register" element={<RegisterPage />} />
+
+        </Route>
     </Routes>
   );
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,23 @@
-export default function LoginPage(options) {
-    return 'login info should be shown here'; 
+import { Link } from "react-router-dom"
+import {useState} from 'react'
+
+export default function LoginPage() {
+    const [email, setEmail] = useState ('')
+    const [password, setPassword] = useState ('')
+
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mt-34">
+            <h1 className="text-4xl text-centermb-4">Login</h1>
+            <form className="max-w-sl max-auto ">
+                <input type="email" placeholder="your@email.com" value= {email} onChange={ev => setEmail (ev.target.value)}/>
+                <input type="password" placeholder="password"  value= {password} onChange={ev => setPassword (ev.target.value)}/>
+                <button className="primary">Login</button>
+                <div className="text-center py-2 text-gray-500">
+                    Don't have an account yet? <Link className="underline text-black" to={"/register"}>Register</Link>
+                </div>
+            </form>
+            </div>
+        </div>
+    )
 }
