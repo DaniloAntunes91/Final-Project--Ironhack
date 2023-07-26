f677a31ecbefcf0b88d2fc1f04aa9590dda07268
“Danilo <danilo.antunes91@gmail.com>
2023-07-26 21:17:54 +0200

    day 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,34 @@ import LoginPage from './pages/loginPage';
 import Layout from './Layout';
 import RegisterPage from './pages/Registerpage';
 import axios from 'axios';
+import {UserContextProvider} from './UserContext';
+import AccountPage from './pages/AccountPage';
+// import { useEffect } from 'react';
 
 axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.withCredentials =true
 function App() {
+
+  
   return (
+
+    <UserContextProvider>
     <Routes>
       <Route path="/" element={<Layout />} >
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/account/:subpage?" element={<AccountPage/>} />
+        <Route path="/account/:subpage?" element={<AccountPage/>} />
+        <Route path="/account/:subpage/:action" element={<AccountPage/>} />
+
+        {/* <Route path="/account/bookings" element={<AccountPage/>} />
+        <Route path="/account/places" element={<AccountPage/>} /> */}
+
 
         </Route>
     </Routes>
+    </UserContextProvider>
   );
 }
 
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom"
+import {useContext} from "react";
+import {UserContext} from "./UserContext"
 export default function Header () {
+   const {user} = useContext (UserContext)
     return (
         <header className="p-4 flex justify-between">
-      <a href="" className="flex items-center gap-1">
+      <Link to ={"/"} href="" className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -17,8 +20,8 @@ export default function Header () {
             d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold text-xl">MyHome</span>
-      </a>
+        <span className="font-bold text-xl">AirBerlin</span>
+      </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className=" border-l border-gray-300"></div>
@@ -44,7 +47,7 @@ export default function Header () {
         </button>
       </div>
       <Link
-        to={"/login"}
+        to={user?"/account":"/login"}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
       >
         <svg
@@ -77,6 +80,11 @@ export default function Header () {
             />
           </svg>
         </div>
+        {!!user && (
+            <div>
+                {user.name}
+            </div>
+        )}
       </Link>
     </header>
     )
